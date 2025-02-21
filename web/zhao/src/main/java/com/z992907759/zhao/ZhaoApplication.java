30cbd4584a9c5b272a0ec1887baa961187e62b20
Goulfrom <992907759@qq.com>
2025-02-21 11:04:47 +0100

    web框架

---
+++ web/zhao/src/main/java/com/z992907759/zhao/ZhaoApplication.java
@@ -0,0 +1,13 @@
+package com.z992907759.zhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhaoApplication.class, args);
+    }
+
+}
