831c8b2242fa2cf28aa163135ef3761a29eda16a
Goulfrom <992907759@qq.com>
2025-03-17 13:19:10 +0100

    Revert "1"
    This reverts commit 2fdabf0f454664119b505475ea0bfce4dc189377.

--- web/zhao/src/main/java/com/z992907759/zhao/mapper/LoginMapper.java
+++
@@ -1,8 +0,0 @@
-package com.z992907759.zhao.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public class LoginMapper {
-
-}
--- web/zhao/src/main/java/com/z992907759/zhao/service/LoginService.java
+++
@@ -1,6 +0,0 @@
-package com.z992907759.zhao.service;
-
-public interface LoginService {
-
-    void login();
-}
